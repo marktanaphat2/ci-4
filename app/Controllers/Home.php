032240368b79eea73b0fd7ba20fc1780c33a3b42
marktanaphat <96627093+marktanaphat@users.noreply.github.com>
2025-01-22 22:06:58 +0700

    update css

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -5,23 +5,27 @@ class Home extends BaseController
 {
     public function index()
     {
-        $data['title'] = "Home Page";
-        $data['data2'] = "data 2";
+        $data['title']  = "Home Page";
+        $data['data2']  = "data 2";
+        $data['active'] = 'home';
         echo view('template/header', $data);
         echo view('template/navbar');
+        echo view('components/slider');
         echo view('welcome_message');
         echo view('template/footer');
     }
     public function about()
     {
-        echo view('template/header');
+        $data['active'] = 'about';
+        echo view('template/header', $data);
         echo view('template/navbar');
         echo view('about');
         echo view('template/footer');
     }
     public function contact()
     {
-        echo view('template/header');
+        $data['active'] = 'contact';
+        echo view('template/header', $data);
         echo view('template/navbar');
         echo view('contact');
         echo view('template/footer');
--- app/Views/template/navbar.php
+++ app/Views/template/navbar.php
@@ -1,6 +1,28 @@
-<div>
-  <a href="/">Home</a> |
-  <a href="/about">About</a> |
-  <a href="/contact">contact</a> |
-  <a href="/register">Register</a>
-</div>
+<header data-bs-theme="dark">
+  <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Carousel</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
+        aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav me-auto mb-2 mb-md-0">
+          <li class="nav-item">
+            <a class="nav-link                               <?php echo $active == 'home' ? 'active' : ''; ?>" aria-current="page" href="/">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/about">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/contact">Contact</a>
+          </li>
+        </ul>
+        <form class=" d-flex" role="search">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
+    </div>
+  </nav>
+</header>
